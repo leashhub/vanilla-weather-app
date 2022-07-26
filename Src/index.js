00619b6b79d002ecf9a9d3423833f2623c84e476
leashhub <leash_prints@outlook.com>
2022-07-25 22:11:02 -0700

    Icons are no longer loading. Starting on my input search function

--- Src/index.js
+++ Src/index.js
@@ -1,16 +1,17 @@
 function formatDate(timestamp) {
   //calculate the date
   let date = new Date(timestamp);
-  // get hours
-  let hours = date.getHours();
-  if (hours > 10) {
-    minutes = `0${hours}`;
-  }
   //get minutes
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
+  // get hours
+  let hours = date.getHours();
+  if (hours > 10) {
+    minutes = `0${hours}`;
+  }
+
   // get day with array
   let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
   let day = days[date.getDay()];
@@ -18,7 +19,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   //get the temperature
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -37,9 +37,24 @@ function displayTemperature(response) {
   // get the current date
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  //change icon
+  let iconElement = document.querySelector("#main-icon");
+  let iconMainElement = response.data.weather[0].icon;
+  iconMainElement.setAttribute = ("src", `/media/${iconElement}.png`);
+}
+
+function search(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-search");
 }
 
 let apiKey = "7dd23c682ad66c852628ad2d2b23df92";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+let city = "Portland";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
+
+let form = document.querySelector("#search-input");
+form.addEventListener("submit", search);
+
+console.log();
